b95330a6e75af4974831249fd2cd7879d509011e
yueyoum <yueyoum@gmail.com>
2014-04-24 10:47:27 +0800

    SecretarySpeech

--- apps/config/admin.py
+++ apps/config/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from apps.config.models import CharInit, ArenaReward, Notify, FunctionOpen,  Dialog, DialogStatement, FunctionDefine
+from apps.config.models import (
+    CharInit,
+    ArenaReward,
+    Notify,
+    FunctionOpen,
+    Dialog,
+    DialogStatement,
+    FunctionDefine,
+    SecretarySpeechType,
+    SecretarySpeech,
+)
 
 class CharInitAdmin(admin.ModelAdmin):
     list_display = (
@@ -61,9 +71,26 @@ class DialogAdmin(admin.ModelAdmin):
     list_filter = ('stage', )
 
 
+
+class SecretarySpeechTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name',
+    )
+
+
+class SecretarySpeechAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'content', 'tp', 'param',
+    )
+
+
+
 admin.site.register(CharInit, CharInitAdmin)
 admin.site.register(ArenaReward, ArenaRewardAdmin)
 admin.site.register(Notify, NotifyAdmin)
 admin.site.register(FunctionDefine, FunctionDefineAdmin)
 admin.site.register(FunctionOpen, FunctionOpenAdmin)
 admin.site.register(Dialog, DialogAdmin)
+admin.site.register(SecretarySpeechType, SecretarySpeechTypeAdmin)
+admin.site.register(SecretarySpeech, SecretarySpeechAdmin)
+
