cf07362e16589c67fd98028e6d7c092dec34d306
yueyoum <yueyoum@gmail.com>
2014-04-25 11:08:41 +0800

    fix

--- apps/errormsg/models.py
+++ apps/errormsg/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class ErrorMsg(models.Model):
     id = models.IntegerField(primary_key=True)
-    error_index = models.CharField(unique=True)
+    error_index = models.CharField(unique=True, max_length=32)
     text_zh = models.CharField("中文", max_length=64)
     des = models.CharField("备注", max_length=64, blank=True)
 
