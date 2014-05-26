63b4064a3a69629b5e73e7fd62fe4e5bb4d4fce2
yueyoum <yueyoum@gmail.com>
2014-05-26 14:48:44 +0800

    add link_to in ErrorMsg

--- apps/errormsg/admin.py
+++ apps/errormsg/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import ErrorMsg
 
 class ErrorMsgAdmin(admin.ModelAdmin):
-    list_display = ('id', 'error_index', 'area', 'text_zh', 'des')
+    list_display = ('id', 'error_index', 'area', 'text_zh', 'des', 'link_to')
     ordering = ('id',)
 
 admin.site.register(ErrorMsg, ErrorMsgAdmin)
--- apps/errormsg/models.py
+++ apps/errormsg/models.py
@@ -3,11 +3,17 @@
 from django.db import models
 
 class ErrorMsg(models.Model):
+    LINK = (
+        (1, 'VIP'),
+        (2, '充值'),
+    )
     id = models.IntegerField(primary_key=True)
     error_index = models.CharField(unique=True, max_length=64)
     area = models.CharField("区域", max_length=16, blank=True)
     text_zh = models.CharField("中文", max_length=64)
     des = models.CharField("备注", max_length=64, blank=True)
+
+    link_to = models.IntegerField("链接", choices=LINK, null=True, blank=True)
 
     class Meta:
         db_table = 'errormsg'
