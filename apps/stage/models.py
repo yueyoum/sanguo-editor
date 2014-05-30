23687060fcbb5ccd3ef01bf92bd47135e107f8c6
yueyoum <yueyoum@gmail.com>
2014-05-30 10:15:59 +0800

    fix

--- apps/stage/models.py
+++ apps/stage/models.py
@@ -90,7 +90,6 @@ class EliteStage(models.Model):
 
     open_condition = models.IntegerField("前置关卡ID")
 
-    first = models.BooleanField("起始")
     next = models.IntegerField("下一关", null=True, blank=True)
 
     monsters = models.TextField("怪物ID")
