e9bfb2dd78f9ba8f6185346edcb3d8641d34a1a2
yueyoum <yueyoum@gmail.com>
2014-11-05 11:09:23 +0800

    Activity

--- apps/activity/admin.py
+++ apps/activity/admin.py
@@ -1,56 +1,65 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
-from apps.activity import models as acm
+from apps.activity.models import ActivityStatic, ActivityStaticCondition
 
 class ActivityStaticAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'tp', 'name', 'start_time', 'continued_days', 'continued_hours'
+        'id', 'name', 'tp', 'mode', 'des', 'condition_type', 'start_time', 'continued_days', 'continued_hours',
+        'current_des', 'conditions',
     )
 
     ordering = ('id',)
 
 
-LIST_DISPLAY = [
-    'id',
-    'activity',
-    'des',
-    'icon_one_type', 'icon_one_id', 'icon_one_amount',
-    'icon_two_type', 'icon_two_id', 'icon_two_amount',
-    'icon_three_type', 'icon_three_id', 'icon_three_amount',
-    'package',
-]
 
-REWARD_FIELDS = [
-    'id',
-    'activity',
-    'des',
-    ('icon_one_type', 'icon_one_id', 'icon_one_amount'),
-    ('icon_two_type', 'icon_two_id', 'icon_two_amount'),
-    ('icon_three_type', 'icon_three_id', 'icon_three_amount'),
-    'package',
-]
+class FilterByActivity(SimpleListFilter):
+    title = ActivityStatic._meta.verbose_name
+
+    parameter_name = 'id__in'
+
+    def lookups(self, request, model_admin):
+        acs = ActivityStatic.objects.all()
+        data = [
+            (ac.conditions, ac.name) for ac in acs
+        ]
+        return tuple(data)
+
+    def queryset(self, request, queryset):
+        values = self.value()
+        if not values:
+            return queryset
+
+        ids = [int(i) for i in values.split(',')]
+        return queryset.filter(id__in=ids)
 
 
-class CharLevelAdmin(admin.ModelAdmin):
-    list_display = LIST_DISPLAY
-    fields = REWARD_FIELDS
 
-class GoodHeroAdmin(admin.ModelAdmin):
-    list_display = LIST_DISPLAY
-    fields = REWARD_FIELDS
+class ActivityStaticConditionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'condition_value',
+        'des',
+        'icon_one_type', 'icon_one_id', 'icon_one_amount',
+        'icon_two_type', 'icon_two_id', 'icon_two_amount',
+        'icon_three_type', 'icon_three_id', 'icon_three_amount',
+        'package',
+    )
 
-class PVEAdmin(admin.ModelAdmin):
-    list_display = LIST_DISPLAY
-    fields = REWARD_FIELDS
+    fields = (
+        'id',
+        'condition_value',
+        'des',
+        ('icon_one_type', 'icon_one_id', 'icon_one_amount'),
+        ('icon_two_type', 'icon_two_id', 'icon_two_amount'),
+        ('icon_three_type', 'icon_three_id', 'icon_three_amount'),
+        'package',
+    )
 
-class PVPAdmin(admin.ModelAdmin):
-    list_display = LIST_DISPLAY
-    fields = REWARD_FIELDS
+    list_filter = (FilterByActivity,)
 
-admin.site.register(acm.ActivityStatic, ActivityStaticAdmin)
-admin.site.register(acm.ActivityStaticCharLevel, CharLevelAdmin)
-admin.site.register(acm.ActivityStaticGoodHero, GoodHeroAdmin)
-admin.site.register(acm.ActivityStaticPVE, PVEAdmin)
-admin.site.register(acm.ActivityStaticPVP, PVPAdmin)
+
+admin.site.register(ActivityStatic, ActivityStaticAdmin)
+admin.site.register(ActivityStaticCondition, ActivityStaticConditionAdmin)
--- apps/activity/models.py
+++ apps/activity/models.py
@@ -11,13 +11,35 @@ class ActivityStatic(models.Model):
         (4, "比武周排名"),
     )
 
-    tp = models.IntegerField("类型", choices=TYPE)
+    MODE = (
+        (1, "手动领取奖励"),
+        (2, "系统发送邮件"),
+    )
+
+    CON_TYPE = (
+        (1, '大于等于'),
+        (2, '小于等于'),
+    )
+
+
+    id = models.IntegerField("ID", primary_key=True)
     name = models.CharField("名字", max_length=64, unique=True)
+    tp = models.IntegerField("类型", choices=TYPE)
+    mode = models.IntegerField("奖励模式", choices=MODE)
+    des = models.TextField("描述", blank=True)
+
+    condition_type = models.IntegerField("条件类型", choices=CON_TYPE)
+
     start_time = models.DateTimeField("开始时间", null=True, blank=True,
                                       help_text="不填写为从开服算起")
+
     continued_days = models.IntegerField("持续天数", default=0)
     continued_hours = models.IntegerField("持续小时数", default=0,
                                           help_text="活动持续总小时为 持续天数*24+持续小时数. 比武周排名只在发送奖励的时候执行一次，忽略持续时间")
+
+    current_des = models.CharField("当前描述", max_length=255)
+
+    conditions = models.CommaSeparatedIntegerField("条件ID", blank=True, max_length=255, help_text='id,id,id')
 
 
     def __unicode__(self):
@@ -25,71 +47,34 @@ class ActivityStatic(models.Model):
 
     class Meta:
         db_table = 'activity_static'
+        ordering = ('id',)
         verbose_name = '固定活动'
         verbose_name_plural = '固定活动'
 
 
-class ActivityReward(models.Model):
+class ActivityStaticCondition(models.Model):
+    id = models.IntegerField("ID", primary_key=True)
+
+    condition_value = models.IntegerField("条件值")
+
     des = models.TextField("描述", blank=True)
 
-    icon_one_type = models.ForeignKey('config.ResourceType', verbose_name='物品1类型', related_name='%(app_label)s_%(class)s_one')
+    icon_one_type = models.ForeignKey('config.ResourceType', verbose_name='物品1类型', related_name='activity_condition_one')
     icon_one_id = models.IntegerField("物品1ID")
     icon_one_amount = models.IntegerField("物品1数量", default=1)
 
-    icon_two_type = models.ForeignKey('config.ResourceType', verbose_name='物品2类型', related_name='%(app_label)s_%(class)s_two')
+    icon_two_type = models.ForeignKey('config.ResourceType', verbose_name='物品2类型', related_name='activity_condition_two')
     icon_two_id = models.IntegerField("物品2ID")
     icon_two_amount = models.IntegerField("物品2数量", default=1)
 
-    icon_three_type = models.ForeignKey('config.ResourceType', verbose_name='物品3类型', related_name='%(app_label)s_%(class)s_three')
+    icon_three_type = models.ForeignKey('config.ResourceType', verbose_name='物品3类型', related_name='activity_condition_three')
     icon_three_id = models.IntegerField("物品3ID")
     icon_three_amount = models.IntegerField("物品3数量", default=1)
 
     package = models.ForeignKey('goodspackage.Package', verbose_name='物品包')
 
     class Meta:
-        abstract = True
+        db_table = 'activity_static_condition'
+        verbose_name = "固定活动-条件"
+        verbose_name_plural = "固定活动-条件"
 
-
-
-class ActivityStaticCharLevel(ActivityReward):
-    id = models.IntegerField("等级级别下限", primary_key=True)
-    activity = models.ForeignKey(ActivityStatic, related_name='activity_static_char_levels', verbose_name='活动')
-
-    class Meta:
-        db_table = 'activity_static_char_level'
-        ordering = ('id',)
-        verbose_name = '固定活动-角色等级'
-        verbose_name_plural = '固定活动-角色等级'
-
-
-class ActivityStaticGoodHero(ActivityReward):
-    id = models.IntegerField("金将数量", primary_key=True)
-    activity = models.ForeignKey(ActivityStatic, related_name='activity_static_good_heros', verbose_name='活动')
-
-    class Meta:
-        db_table = 'activity_static_good_hero'
-        ordering = ('id',)
-        verbose_name = '固定活动-武将召唤'
-        verbose_name_plural = '固定活动-武将召唤'
-
-
-class ActivityStaticPVE(ActivityReward):
-    id = models.ForeignKey('stage.Battle', primary_key=True, verbose_name='战役')
-    activity = models.ForeignKey(ActivityStatic, related_name='activity_static_pves', verbose_name='活动')
-
-    class Meta:
-        db_table = 'activity_static_pve'
-        ordering = ('id',)
-        verbose_name = '固定活动-通过战役'
-        verbose_name_plural = '固定活动-通过战役'
-
-
-class ActivityStaticPVP(ActivityReward):
-    id = models.IntegerField("名次级别下限", primary_key=True)
-    activity = models.ForeignKey(ActivityStatic, related_name='activity_static_pvps', verbose_name='活动')
-
-    class Meta:
-        db_table = 'activity_static_pvp'
-        ordering = ('id',)
-        verbose_name = '固定活动-比武周排名'
-        verbose_name_plural = '固定活动-比武周排名'
