6bbc5b0cfe85ff3acf4c91bc259df3e9ebd885fb
yueyoum <yueyoum@gmail.com>
2014-12-03 17:15:08 +0800

    UnionCheckin

--- apps/union/admin.py
+++ apps/union/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from apps.union.models import UnionStore
+from apps.union.models import UnionStore, UnionCheckin
 
 class UnionStoreAdmin(admin.ModelAdmin):
     list_display = ('id', 'tp', 'value', 'des', 'union_coin')
 
 
+class UnionCheckinAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cost_type', 'cost_value', 'got_contributes', 'got_coin')
+
 admin.site.register(UnionStore, UnionStoreAdmin)
+admin.site.register(UnionCheckin, UnionCheckinAdmin)
 
--- apps/union/models.py
+++ apps/union/models.py
@@ -16,3 +16,21 @@ class UnionStore(models.Model):
         verbose_name = '工会商店'
         verbose_name_plural = '工会商店'
 
+
+class UnionCheckin(models.Model):
+    COST_TYPE = (
+        (1, '银两'),
+        (2, '元宝'),
+    )
+
+    id = models.IntegerField("签到次数", primary_key=True)
+    cost_type = models.IntegerField("消耗类型", choices=COST_TYPE)
+    cost_value = models.IntegerField("消耗数值")
+
+    got_contributes = models.IntegerField("获取贡献")
+    got_coin = models.IntegerField("获取工会币")
+
+    class Meta:
+        db_table = 'union_checkin'
+        verbose_name = "工会签到"
+        verbose_name_plural = "工会签到"
