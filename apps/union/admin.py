ea23794d886ad5a8e147129cece5ceb5e271497c
yueyoum <yueyoum@gmail.com>
2014-12-03 14:35:41 +0800

    .

--- apps/union/admin.py
+++ apps/union/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from apps.union.models import UnionStore
 
 class UnionStoreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'tp', 'used_for', 'value', 'des', 'union_coin')
+    list_display = ('id', 'tp', 'value', 'des', 'union_coin')
 
 
 admin.site.register(UnionStore, UnionStoreAdmin)
--- apps/union/models.py
+++ apps/union/models.py
@@ -4,24 +4,9 @@ from django.db import models
 
 
 class UnionStore(models.Model):
-    TYPE = (
-        (1, 'BUFF'),
-        (2, '马'),
-        (3, '道具'),
-    )
-
-    BUFF = (
-        ('attack', '攻击'),
-        ('defense', '防御'),
-        ('hp', '生命')
-    )
-
-
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=32)
-    tp = models.IntegerField("类型", choices=TYPE)
+    tp = models.ForeignKey('config.ResourceType', verbose_name='类型')
 
-    used_for = models.CharField("BUFF用于", choices=BUFF, max_length=32, blank=True, help_text='非BUFF不用选择')
     value = models.IntegerField("值", blank=True, null=True, help_text='buff的值，或者其他物品的ID')
     des = models.TextField("说明", blank=True)
     union_coin = models.IntegerField("所需工会币")
